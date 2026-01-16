d01326ce654e71cff07557e672f4c16ae35a2be6
Piyush Kamlakar <you@example.com>
2026-01-16 21:26:11 +0530

    added the merge logic to merge codeforces and leetcode data

--- scrape.js
+++ scrape.js
@@ -168,7 +168,7 @@ async function scrapeCodeforces() {
 }
 
 
-// scrapeLeetcodeProblems();
+scrapeLeetcodeProblems();
 scrapeCodeforces();
 
 
---
+++ utils/merge.js
@@ -0,0 +1,29 @@
+import fsPromises from "fs/promises";
+import path from "path";
+import { json } from "stream/consumers";
+
+async function mergeProblemData(){
+    const codeforcesPath = path.resolve("./problems/codeforces_problems.json");
+    const leetcodePath = path.resolve("./problems/leetcode_problems.json");
+
+    const leetcodeData = JSON.parse(await fsPromises.readFile(leetcodePath,"utf-8"));
+    const codeforcesData = JSON.parse(await fsPromises.readFile(codeforcesPath,"utf-8"));
+
+    const combined = [...codeforcesData,...leetcodeData];
+    await fsPromises.mkdir("./corpus",{recursive:true});
+
+    await fsPromises.writeFile(
+        "./corpus/all_problems.json",
+        JSON.stringify(combined,null,2)
+    )
+
+}
+
+
+
+
+
+
+
+
+mergeProblemData();
